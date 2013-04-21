3c12125442fe781265f5c607c1202e376e4643f7
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-21 16:51:50 -0300

    typo!

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 heroku apps:create --buildpack https://github.com/ArchiveTeam/heroku-buildpack-archiveteam-warrior.git
-heroku config:set WARRIOR_PROJECT=httpstps://github.com/ArchiveTeam/yahoo-upcoming-grab
+heroku config:set WARRIOR_PROJECT=https://github.com/ArchiveTeam/yahoo-upcoming-grab
 heroku config:set WARRIOR_PROJECTRIOR_DOWNLOADER=mroth-upcoming-cloud-warrior
 git push heroku master
 heroku ps:scale seesaw=1
