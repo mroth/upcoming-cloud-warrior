387e0f57f0df60d3acb393b0604c2495f8b4476e
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-21 15:53:50 -0400

    wtf with the typos

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 heroku apps:create --buildpack https://github.com/ArchiveTeam/heroku-buildpack-archiveteam-warrior.git
 heroku config:set WARRIOR_PROJECT=https://github.com/ArchiveTeam/yahoo-upcoming-grab
-heroku config:set WARRIOR_PROJECTRIOR_DOWNLOADER=mroth-upcoming-cloud-warrior
+heroku config:set WARRIOR_DOWNLOADER=mroth-upcoming-cloud-warrior
 git push heroku master
 heroku ps:scale seesaw=1
 echo "All done!  You can view the progress by running 'heroku logs --tail'"
-
